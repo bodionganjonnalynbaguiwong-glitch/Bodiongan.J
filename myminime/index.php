9eb0c8dc553d8fc92c860466d7004495ca28f523
bodionganjonnalynbaguiwong-glitch <bodionganjonnalynbaguiwong@gmail.com>
2026-03-23 18:51:16 +0800

    Add files via upload

---
+++ myminime/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Jonnalyn B. Bodiongan</h1>
+    <div class="container">
+    <label for="uname"><b>Username:</b></label>
+    <input type="text" placeholder="Enter Username" name="urname" required>
+
+    <label for="psw"><b>Password:</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
+    <button type="submit">Login</button>
+  </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+<div>    
+</body>
+</html>
