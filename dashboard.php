fc3bdae532e889e4bbc3fb8924a8920ae80ce555
bodionganjonnalynbaguiwong-glitch <bodionganjonnalynbaguiwong@gmail.com>
2026-04-06 19:04:17 +0800

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+if(!isset($p_SESSION['user'])){
+    header("Location: index.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <a ref="index.php"></a>
+</head>
+<body>
+    <h1>Welcome to my Page</h1>
+</body>
+</html>
